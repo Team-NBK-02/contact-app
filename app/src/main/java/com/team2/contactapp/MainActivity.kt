457dd9b62785765d40b5c2c6af85bb4ec107e6aa
PotatoMeme <kshk0897@gmail.com>
2023-09-04 16:40:56 +0900

    [FEAT] : ⚙️ 뷰바인딩 추가및 적용

--- app/src/main/java/com/team2/contactapp/MainActivity.kt
+++ app/src/main/java/com/team2/contactapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.team2.contactapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.team2.contactapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
