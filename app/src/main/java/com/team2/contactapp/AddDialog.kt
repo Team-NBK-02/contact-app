48d120ec093896effe64509d04ae659f64fa0b0e
PotatoMeme <kshk0897@gmail.com>
2023-09-07 16:18:13 +0900

    [LITTLE_FIX] : 🔧 add dialog

--- app/src/main/java/com/team2/contactapp/AddDialog.kt
+++ app/src/main/java/com/team2/contactapp/AddDialog.kt
@@ -1,9 +1,14 @@
 package com.team2.contactapp
 
+import android.content.Context
+import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.DialogFragment
 import com.team2.contactapp.databinding.DialogAddBinding
 
@@ -36,6 +41,39 @@ class AddDialog(private val addDialogInterface: AddDialogInterface) : DialogFrag
         saveButton.setOnClickListener {
             addDialogInterface.onSaveButtonClicked()
             dismiss()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireContext().dialogFragmentResize(this, 0.9f, 0.7f)
+    }
+
+    fun Context.dialogFragmentResize(dialogFragment: DialogFragment, width: Float, height: Float) {
+        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        if (Build.VERSION.SDK_INT < 30) {
+
+            val display = windowManager.defaultDisplay
+            val size = Point()
+
+            display.getSize(size)
+
+            val window = dialogFragment.dialog?.window
+
+            val x = (size.x * width).toInt()
+            val y = (size.y * height).toInt()
+            window?.setLayout(x, y)
+
+        } else {
+
+            val rect = windowManager.currentWindowMetrics.bounds
+
+            val window = dialogFragment.dialog?.window
+
+            val x = (rect.width() * width).toInt()
+            val y = (rect.height() * height).toInt()
+
+            window?.setLayout(x, y)
         }
     }
 
